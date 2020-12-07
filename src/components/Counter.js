45e392d12e889c286d0eeff089008b8b1d2f2c31
kenckl <klcheung8355@gmail.com>
2020-12-07 17:55:12 +0800

    counter

---
+++ src/components/Counter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            number: 0,
+        };
+    }
+
+    onIncrease = () => {
+        this.setState(prevState => ({number: prevState.number+1}));
+    };
+
+    onDecrease = () => {
+        this.setState(prevState => ({number: prevState.number-1}));
+    };
+    
+    render() {
+        return (
+            <div>
+                <input type="button" value="+" onClick={this.onIncrease} />
+                {this.state.number}
+                <input type="button" value="-" onClick={this.onDecrease} />
+            </div>
+        );
+    }
+}
